edf283a557499444a9fa666ac5a79cbc8a2a77ba
dipakkobiya <kobiyadipak@gmail.com>
2017-07-18 15:02:00 +0530

    Initial commit

---
+++ app/src/main/java/demo/list/dynamic/nkdroid/com/niravdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package demo.list.dynamic.nkdroid.com.niravdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //dipak nirav
+
+        //demo name
+        //today
+
+        //nirav patel
+        //rajesh bhai
+
+    }
+}
